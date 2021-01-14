6723366d1518ec6476bf0835a5018cb79f23fbcf
JaySRT <48157133+JaySRT@users.noreply.github.com>
2021-01-13 23:16:33 -0500

    Update Keyword_based_search.py

--- Keyword_based_search.py
+++ Keyword_based_search.py
@@ -1,1 +1,16 @@
 
+
+
+
+#The search term we want to find
+query = "Focus"
+#Language code (follows ISO 639-1 standards)
+language = "en"
+
+#Calling the user_timeline function with our parameters
+results = api.search(q=query, lang=language)
+
+#Printing all tweets (for each through all tweets pulled)
+for tweet in results:
+    print(tweet.user.screen_name,"Tweeted:",tweet.text)
+
