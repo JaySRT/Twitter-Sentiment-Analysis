36658c873e0dbfb9c460375e73b50cf9e023f3d5
JaySRT <48157133+JaySRT@users.noreply.github.com>
2021-01-13 23:18:16 -0500

    Update Keyword_based_search.py

--- Keyword_based_search.py
+++ Keyword_based_search.py
@@ -1,8 +1,8 @@
 import tweepy 
-consumer_key = ""
-consumer_secret = ""
-access_token = ""
-access_token_secret = ""
+consumer_key = "entering the key here"
+consumer_secret = "entering the key here"
+access_token = "entering the key here"
+access_token_secret = "entering the key here"
 
 
 from tweepy.auth import OAuthHandler
@@ -12,8 +12,6 @@ auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 #Creating the API object while passing in the auth info
 api = tweepy.API(auth)
-
-
 
 
 #The search term we want to find
@@ -27,4 +25,3 @@ results = api.search(q=query, lang=language)
 #Printing all tweets (for each through all tweets pulled)
 for tweet in results:
     print(tweet.user.screen_name,"Tweeted:",tweet.text)
-
