7efc805ffd44d1e7864aaceedb69be40542606ef
JaySRT <48157133+JaySRT@users.noreply.github.com>
2021-01-16 09:30:51 -0500

    Update Sentiment_analysis.py

--- Sentiment_analysis.py
+++ Sentiment_analysis.py
@@ -1,10 +1,11 @@
+#Codes for terminal before we start with the analysis
+#mkdir how-positive-was-your-week
+#cd how-positive-was-your-week
+#touch week.py
+#touch config.yaml
+
 import requests
 import pandas as pd
 import json
 import ast
 import yaml
-
-#mkdir how-positive-was-your-week
-#cd how-positive-was-your-week
-#touch week.py
-#touch config.yaml
