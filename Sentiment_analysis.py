ad481642c04a78478fbf67457a3715647b9d0c9e
JaySRT <48157133+JaySRT@users.noreply.github.com>
2021-01-18 22:32:29 -0500

    Update Sentiment_analysis.py

--- Sentiment_analysis.py
+++ Sentiment_analysis.py
@@ -9,3 +9,14 @@ import pandas as pd
 import json
 import ast
 import yaml
+
+def create_twitter_url():
+    handle = "jaysorathi"
+    max_results = 100
+    mrf = "max_results={}".format(max_results)
+    q = "query=from:{}".format(handle)
+    url = "https://api.twitter.com/2/tweets/search/recent?{}&{}".format(
+        mrf, q
+    )
+    return url
+  
